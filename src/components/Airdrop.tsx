f5aeea5df6209107a3844823029d0394f8bf9a42
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-09-07 22:47:55 +0530

    airdop issue fixed

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -12,6 +12,7 @@ const Airdrop = () => {
   const { connection } = useConnection();
   const [amount, setAmount] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
 
   const sendAirdrop = async () => {
     if (wallet.connected) {
@@ -24,13 +25,11 @@ const Airdrop = () => {
         return;
       }
       setError("");
-      console.log(
-        "Sending airdrop of",
-        parseFloat(amount) * 1000000000,
-        "SOL to",
-        wallet.publicKey
+      await connection.requestAirdrop(
+        wallet.publicKey!,
+        parseFloat(amount) * 1000000000
       );
-      // await connection.requestAirdrop(wallet.publicKey!, amount * 1000000000);
+      setSuccess(true);
     } else {
       setError("Please connect your wallet to send an airdrop");
     }
@@ -38,14 +37,17 @@ const Airdrop = () => {
 
   return (
     <>
-      <div className="flex flex-col sm:flex-row gap-2">
-        <input
-          type="text"
-          placeholder="Enter Amount"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          className="px-4 py-2 text-black outline-none rounded-l-sm"
-        />
+      <div className="flex flex-col sm:flex-row gap-2 items-center">
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Enter Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="w-36 px-4 py-2 text-black outline-none rounded-l-sm text-center"
+          />
+          <p className="bg-white text-gray-500 px-4 py-2">SOL</p>
+        </div>
         <button
           className="bg-violet-900 hover:bg-violet-800 px-4 py-2 rounded-r-sm"
           onClick={sendAirdrop}
@@ -54,7 +56,7 @@ const Airdrop = () => {
         </button>
       </div>
       {error && <p className="text-red-300 font-semibold">{error}</p>}
-      <SuccessDialog />
+      {success && <SuccessDialog />}
     </>
   );
 };
